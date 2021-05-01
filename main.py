781ae35f557785119cd02874e0cfec990b588d02
izahonorato <izahonorato@gmail.com>
2021-05-01 13:53:55 -0300

    Primeiro programa em python

---
+++ main.py
@@ -0,0 +1,10 @@
+def olaMundo(nome):
+    print(f'Olá mundo, {nome}')
+
+if __name__ == '__main__':
+    numero = input("Digite um número:")
+    print(f'O número digitado foi {numero}')
+
+#comentario no python
+variavelNula = None #variável nula, como se fosse o Null de outras linguagens
+
