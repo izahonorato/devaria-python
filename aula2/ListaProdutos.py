39f64534d7a06e8f3f2c3c0559f9fd27c378a75c
izahonorato <izahonorato@gmail.com>
2021-05-09 14:48:39 -0300

    Trabalhando com Listas

---
+++ aula2/ListaProdutos.py
@@ -0,0 +1,25 @@
+if __name__ == '__main__':
+    produtos = []
+    produtosMercado = ['Refrigerante', 'Cerveja', 'Maçã', 'Arroz', 'Carne']
+
+    while True:
+        produto = input("Digite um produto para adicionar na lista (Digite sair para encerrar): ")
+
+        if produto == 'sair':
+            break
+        produtos.append(produto)
+
+    produtosIndisponiveis = []
+    produtosDisponiveis = []
+
+    for p in produtos:
+        if not p in produtosMercado:
+            produtosIndisponiveis.append(p)
+        else:
+            produtosDisponiveis.append(p)
+
+    print(f'Estes produtos nós temos: {produtosDisponiveis}')
+    print(f'Estes produtos nós não temos: {produtosIndisponiveis}')
+
+    produtosDisponiveis.sort()
+    print(f'Produtos disponíveis em ordem alfabética: {produtosDisponiveis}')
--- aula2/Listas-Funções-Anonimas-Tratamento-Exceção.py
+++ aula2/Listas-Funções-Anonimas-Tratamento-Exceção.py
@@ -1,2 +1,36 @@
 if __name__ == '__main__':
+
     #tratamento de exceção com try except
+    try:
+        pessoas = []
+        while True:
+            nome = input("Digite um nome para adicionar na lista e digite sair para parar: ")
+
+            if nome == 'sair':
+                break
+
+            pessoas.append(nome)
+
+        print("Pessoas na lista: ")
+        print(pessoas)
+
+        pessoas.sort() #.sort(reverse=True) vai voltar em ordem alfabética ao contrario
+
+        print(pessoas)
+
+        #insere objetos em uma posição especifica, empurrando os outros pra frente
+        pessoas.insert(2, "Sara")
+        print(pessoas)
+
+        print(f'Quantidade de Ana na lista {pessoas.count("Ana")}')
+
+        notasAlunos = [50, 60, 35, 97, 77]
+        mediaNotas = lambda notas: sum(notas) / len(notas) #função anônima ou lambda
+
+        print(f'A media das notas dos alunos é : {mediaNotas(notasAlunos)}')
+
+        notasFiltradas = list(filter(lambda nota : nota > 80, notasAlunos))
+        print(f'Notas filtradas acima de 80: {notasFiltradas}')
+
+    except Exception as e:
+        print("Erro")
